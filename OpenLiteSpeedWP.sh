71541ddba7255895c34b583ca750199c48bb3574
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2024-05-18 01:58:33 +0500

    Update OpenLiteSpeedWP.sh
    Added firewall rules

--- OpenLiteSpeedWP.sh
+++ OpenLiteSpeedWP.sh
@@ -5,5 +5,7 @@ apt -y install sudo wget curl
 bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/wpimgsetup.sh )
 ### Regenerate password for Web Admin, Database, setup Welcome Message
 bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
+## Allow ports in firewall rules
+sudo ufw allow 7080/tcp
 ### Reboot server
 reboot
